67bd3fcb82be17ca9ce609d0c4b1e4e048460f5b
Leandro_SmS <leandrosimoesms@gmail.com>
2023-02-08 09:40:04 -0300

    added price_time component

--- nft-preview-card-component-main/main.js
+++ nft-preview-card-component-main/main.js
@@ -5,20 +5,48 @@ const data = [
     {
         heading: "Equilibrium #3429",
         text: "Our Equilibrium collection promotes balance and calm.",
-        author: "Leandro Simões"
+        author: "Leandro Simões",
+        price_time: {
+            price: 0.041,
+            time: 3
+        }
+
     }
 ]
 
 
 //  Components
 
+function Price_Time (props) {
+    return (
+        <div className="price-time">
+            <div className="price-div">
+                <img className="eth-icon" src="./images/icon-ethereum.svg" />
+                <p className="price">{props.price}ETH</p>
+            </div>
+            <div className="time-div">
+                <img className="time-icon" src="./images/icon-clock.svg" />
+                <p className="time" >{props.time} days left</p>
+            </div>
+        </div>
+    )
+}
+
 function Main (props) {
+
+    const price_time = data.map((obj) => {
+        return <Price_Time 
+                    price={obj.price_time.price}
+                    time={obj.price_time.time} />
+    });
+
     return (
         <main>
-            <div clasName="nft">
+            <div className="nft">
                 <img/>
                 <h1 className="heading">{props.heading}</h1>
-                <p clasName="text">{props.text}</p>
+                <p className="text">{props.text}</p>
+                {price_time}
             </div>
         </main>
     )
