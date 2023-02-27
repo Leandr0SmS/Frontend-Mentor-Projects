bd0aca06c0ab1f27cc054d36751a0512541771d2
Leandro_SmS <leandrosimoesms@gmail.com>
2023-02-27 06:20:01 -0300

    main.js: Created Question component; adde faqData array; main.scss: added font-family and background color

--- faq-accordion-card/main.js
+++ faq-accordion-card/main.js
@@ -1,26 +1,51 @@
-/* How many team members can I invite?
-  You can invite up to 2 additional users on the Free plan. There is no limit on 
-  team members for the Premium plan.
+const faqData = [
+  {
+    question: "How many team members can I invite?",
+    answer: "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan."
+  },
+  {
+    question: "What is the maximum file upload size?",
+    answer: "No more than 2GB. All files in your account must fit your allotted storage space."
+  },
+  {
+    question: "How do I reset my password?",
+    answer: "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you."
+  },
+  {
+    question: "Can I cancel my subscription?",
+    answer: "Yes! Send us a message and we’ll process your request no questions asked."
+  },
+  {
+    question: "Do you provide additional support?",
+    answer: "Chat and email support is available 24/7. Phone lines are open during normal business hours."
+  }
+]
 
-  What is the maximum file upload size?
-  No more than 2GB. All files in your account must fit your allotted storage space.
 
-  How do I reset my password?
-  Click “Forgot password” from the login page or “Change password” from your profile page.
-  A reset link will be emailed to you.
-
-  Can I cancel my subscription?
-  Yes! Send us a message and we’ll process your request no questions asked.
-
-  Do you provide additional support?
-  Chat and email support is available 24/7. Phone lines are open during normal business hours.
- */
-
+function Questions() {
+  return (
+    <div className="questions--div">
+      <h3 className="questions">somethig to question?</h3>
+      <p className="answer">Response</p>
+    </div>
+  )
+}
 
 //App
+
+
+
 function App() {
   return (
-    <h1>FAQ</h1>
+    <div className="faq--card">
+      <div className="image--div">
+        <img src=".\images\illustration-box-desktop.svg" alt="Cardboard box with an '@' on top"/>
+      </div>
+      <div className="faq--div">
+        <h1>FAQ</h1>
+        < Questions/>
+      </div>
+    </div>
   )
 }
 
