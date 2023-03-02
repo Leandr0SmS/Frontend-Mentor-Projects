75f329f3b9150de5998d893dab8462504881ff45
Leandro_SmS <leandrosimoesms@gmail.com>
2023-03-02 06:06:36 -0300

    added scripts to index.html; Created main.js: Added render code, added text as comments

---
+++ pod-request-access-landing-page/starter-code/main.js
@@ -0,0 +1,19 @@
+/*  
+Publish your podcasts everywhere.
+
+Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, 
+Apple Podcasts, Google Podcasts, Pocket Casts and more!
+
+Request access 
+*/
+
+function App() {
+    return (
+        <h1>hello</h1>
+    )
+}
+
+// Render
+const app = document.getElementById('root');
+const root = ReactDOM.createRoot(app);
+root.render(<App />)
