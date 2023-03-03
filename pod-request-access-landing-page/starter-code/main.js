d5c1ec25631fe028e823ced6e47474dd9e96c4a0
Leandro_SmS <leandrosimoesms@gmail.com>
2023-03-03 05:59:10 -0300

    main.js: Added div with informations

--- pod-request-access-landing-page/starter-code/main.js
+++ pod-request-access-landing-page/starter-code/main.js
@@ -9,7 +9,27 @@ Request access
 
 function App() {
     return (
-        <h1>hello</h1>
+        <div className="app">
+            <header>
+                <img alt="logo-pod" src=".\assets\desktop\logo.svg"/>
+            </header>
+            <main className="main">
+                <h1>
+                    Publish your podcasts <span>everywhere.</span>
+                </h1>
+                <p>
+                    Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, 
+                    Apple Podcasts, Google Podcasts, Pocket Casts and more!
+                </p>
+                <button/>
+                <div className="podcasts--apps">
+                    <img src="./assets/desktop/spotify.svg"/>
+                    <img src="./assets/desktop/apple-podcast.svg"/>
+                    <img src="./assets/desktop/google-podcasts.svg"/>
+                    <img src="./assets/desktop/pocket-casts.svg"/>
+                </div>
+            </main>
+        </div>
     )
 }
 
