d5e7ee8568b6b725f5d93c9fcc113157d5b31036
Leandro_SmS <leandrosimoesms@gmail.com>
2023-03-04 06:07:20 -0300

    main.scss: Added @media to tablet screens

--- pod-request-access-landing-page/starter-code/main.js
+++ pod-request-access-landing-page/starter-code/main.js
@@ -1,11 +1,3 @@
-/*  
-Publish your podcasts everywhere.
-
-Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, 
-Apple Podcasts, Google Podcasts, Pocket Casts and more!
-
-Request access 
-*/
 
 function App() {
     return (
