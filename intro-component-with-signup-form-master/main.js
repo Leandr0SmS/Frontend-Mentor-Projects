84475d4cdff3c308fb854ccba832b820a4c2ae9d
Leandro_SmS <leandrosimoesms@gmail.com>
2023-03-07 12:00:55 -0300

    main.js: Created JSX; main.scss: Added background image and color

--- intro-component-with-signup-form-master/main.js
+++ intro-component-with-signup-form-master/main.js
@@ -1,23 +1,50 @@
-/*  Learn to code by watching others
-
-  See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
-  but understanding how developers think is invaluable. 
-
-  Try it free 7 days then $20/mo. thereafter
-
-  First Name 
-  Last Name 
-  Email Address
-  Password
-
-  Claim your free trial 
-
-  By clicking the button, you are agreeing to our Terms and Services */
-
 function App() {
-    return (
-        <h1>hello</h1>
-    )
+  return (
+    <div className="app">
+      <div className="Heading">
+        <h1>
+          Learn to code by watching others
+        </h1>
+        <p>  
+          See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+          but understanding how developers think is invaluable.
+        </p>
+      </div>
+      <div className="main">
+        <div className="try-free">
+          <p>
+            <span>Try it free 7 days</span> then $20/mo. thereafter
+          </p>
+        </div>
+        <form className="form">
+          <input
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+          />
+          <input
+            type="text"
+            placeholder="Email Address"
+            name="email"
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            name="password"
+          />
+          <button>
+            Claim your free trial 
+          </button>
+          <p> By clicking the button, you are agreeing to our<a>Terms and Services</a></p>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 //render
