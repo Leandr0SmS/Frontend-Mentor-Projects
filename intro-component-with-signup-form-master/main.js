1093035a276ada45030871c3ff6a82ad07f5b422
Leandro_SmS <leandrosimoesms@gmail.com>
2023-03-07 17:46:54 -0300

    main.js: Added handleChange function; index.html: removed duplicated script

--- intro-component-with-signup-form-master/main.js
+++ intro-component-with-signup-form-master/main.js
@@ -1,3 +1,12 @@
+//email validation function
+function emailValidation(email) {
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email) && true; 
+}
+function handleSubmit(e) {
+  e.preventDefault();
+}
+
 function App() {
   const [formData, setFormData] = React.useState(
     {
@@ -6,7 +15,29 @@ function App() {
       email: "",
       password: ""
     }
-  )
+  );
+  function handleChange(event) {
+    const {name, value} = event.target;
+    setFormData((prevFormState) =>({
+      ...prevFormState,
+      [name]: value
+    }))
+  }
+  function formValidation() {
+    emailValidation(formData.email)
+      ? console.log("email ok")
+      : console.log("email not");
+    formData.firstName
+      ? console.log("name ok")
+      : console.log("name not");
+    formData.lastName
+    ? console.log("name ok")
+    : console.log("name not");
+    formData.password
+    ? console.log("password ok")
+    : console.log("password not")
+  }
+
   return (
     <div className="app">
       <div className="Heading">
@@ -24,28 +55,41 @@ function App() {
             <span>Try it free 7 days</span> then $20/mo. thereafter
           </p>
         </div>
-        <form className="form">
+        <form 
+          className="form"
+          onSubmit={handleSubmit}
+        >
           <input
             type="text"
             placeholder="First Name"
             name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
           />
           <input
             type="text"
             placeholder="Last Name"
             name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
           />
           <input
             type="text"
             placeholder="Email Address"
             name="email"
+            value={formData.email}
+            onChange={handleChange}
           />
           <input
             type="text"
             placeholder="Password"
             name="password"
+            value={formData.password}
+            onChange={handleChange}
           />
-          <button>
+          <button
+            className="submit--btn"
+          >
             Claim your free trial 
           </button>
           <p> By clicking the button, you are agreeing to our<a>Terms and Services</a></p>
@@ -55,7 +99,7 @@ function App() {
   )
 }
 
-//render
+// Render
 const app = document.getElementById('root');
 const root = ReactDOM.createRoot(app);
 root.render(<App />)
