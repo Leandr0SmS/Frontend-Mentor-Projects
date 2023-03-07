f4ae88ace0f34ce8e74713cdf8b0b0cccbe056e1
Leandro_SmS <leandrosimoesms@gmail.com>
2023-03-07 06:26:22 -0300

    main.js: Added render code

---
+++ intro-component-with-signup-form-master/main.js
@@ -0,0 +1,26 @@
+/*  Learn to code by watching others
+
+  See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+  but understanding how developers think is invaluable. 
+
+  Try it free 7 days then $20/mo. thereafter
+
+  First Name 
+  Last Name 
+  Email Address
+  Password
+
+  Claim your free trial 
+
+  By clicking the button, you are agreeing to our Terms and Services */
+
+function App() {
+    return (
+        <h1>hello</h1>
+    )
+}
+
+//render
+const app = document.getElementById('root');
+const root = ReactDOM.createRoot(app);
+root.render(<App />)
