7ab6a74e8886edfdcf879077a3814fbedb5bf535
Leandro_SmS <leandrosimoesms@gmail.com>
2023-03-25 06:28:35 -0300

    main.js: Set elements: base.scss: Set colors variables

--- advice-generator-app-main/main.js
+++ advice-generator-app-main/main.js
@@ -7,9 +7,34 @@ function App() {
             .then(data => setData(data.slip))
     }, [get])
     return (
-        <div>
-            <h1>{data.advice}</h1>
-            <button onClick={() => setGet(c => c + 1)}>New Advise: {`${get}`}</button>
+        <div className="container">
+            <p className="advice--id">{data.id}</p>
+            <h2 className="advice">{data.advice}</h2>
+            <picture>
+                <source 
+                    media="(max-width:750px)" 
+                    srcSet="./images/pattern-divider-mobile.svg"
+                    alt="A line under the advice"
+                    aria-hidden="false"
+                    role="img"
+                />
+                <img
+                    className="divider"
+                    src="./images/pattern-divider-desktop.svg"
+                    alt="A line under the advice"
+                    aria-hidden="false"
+                    role="img"
+                />
+            </picture>
+            <div className="buttom" onClick={() => setGet(c => c + 1)}>
+                <img
+                    className="dice--icon"
+                    src="./images/icon-dice.svg"
+                    alt="Small dice"
+                    aria-hidden="false"
+                    role="icon"
+                />
+            </div>
         </div>
     )
 }
