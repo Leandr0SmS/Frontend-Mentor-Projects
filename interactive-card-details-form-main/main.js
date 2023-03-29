2306527b8901e0ea6defe76cd488f7b1e6b6ffcd
Leandro_SmS <leandrosimoesms@gmail.com>
2023-03-29 07:25:16 -0300

    Set components <Form/> and <Cards/>

--- interactive-card-details-form-main/main.js
+++ interactive-card-details-form-main/main.js
@@ -1,114 +1,102 @@
 /*
-  0000 0000 0000 0000
-  Jane Appleseed
-  00/00
-
-  000
-
-  Cardholder Name
-  e.g. Jane Appleseed
-
-  Card Number
-  e.g. 1234 5678 9123 0000
-
-  Exp. Date (MM/YY)
-  MM
-  YY
-
-  CVC
-  e.g. 123
-
-  Confirm
-
-  <!-- Completed state start -->
-
   Thank you!
   We've added your card details
   Continue
 */
+function Cards() {
+    return (
+        <div className="cards--div">
+            <div className="card--front--div">
+                <img
+                    className="card--logo"
+                    src="./images/card-logo.svg"
+                    alt="card logo"
+                    aria-hidden="false"
+                    role="img"
+                />
+                <h2 className="card--number">0000 0000 0000 0000</h2>
+                <div className="card--name--date--div">
+                    <p className="card--name">Jane Appleseed</p>
+                    <p className="card--expDate">00/00</p>
+                </div>
+            </div>
+            <div className="card--back--div">
+                <p className="card--CVC">000</p>
+            </div>
+        </div>
+    )
+}
+
+function Form() {
+    return (
+        <form className="form">
+            <label 
+                className="label"
+                htmlFor="name"
+            >Cardholder Name</label>
+            <input
+                className="input"
+                name="name"
+                id="name"
+                placeholder="e.g. Jane Appleseed"
+            />
+            <label 
+                htmlFor="number"
+                className="label"
+            >Card Number</label>
+            <input
+                className="input"
+                name="number"
+                id="number"
+                placeholder="e.g. 1234 5678 9123 0000"
+            />
+            <div className="form--expDate--cvc--div">
+                <div className="form--expDate--label--div">
+                    <label
+                        className="label"
+                    >Exp. Date (MM/YY)</label>
+                    <div className="form--expDate--div">
+                        <input
+                            className="input"
+                            name="MM"
+                            id="MM"
+                            placeholder="MM"
+                        />
+                        <input
+                            className="input"
+                            name="YY"
+                            id="YY"
+                            placeholder="YY"
+                        />
+                    </div>
+                </div>
+                <div className="form--CVC--div">
+                    <label
+                        className="label" 
+                        htmlFor="e.g."
+                    >CVC</label>
+                    <input
+                        className="input"
+                        name="eg"
+                        id="eg"
+                        placeholder="e.g. 123"
+                    />
+                </div>
+            </div>
+            <button
+                className="btn"
+                id="btn"
+                name="btn"
+            >Confirm</button>
+        </form>
+    )
+}
 
 function App() {
     return (
         <div className="app">
-            <div className="cards--div">
-                <div className="card--front--div">
-                    <img
-                        className="card--logo"
-                        src="./images/card-logo.svg"
-                        alt="card logo"
-                        aria-hidden="false"
-                        role="img"
-                    />
-                    <h2 className="card--number">0000 0000 0000 0000</h2>
-                    <div className="card--name--date--div">
-                        <p className="card--name">Jane Appleseed</p>
-                        <p className="card--expDate">00/00</p>
-                    </div>
-                </div>
-                <div className="card--back--div">
-                    <p className="card--CVC">000</p>
-                </div>
-            </div>
-            <form className="form">
-                <label 
-                    className="label"
-                    htmlFor="name"
-                >Cardholder Name</label>
-                <input
-                    className="input"
-                    name="name"
-                    id="name"
-                    placeholder="e.g. Jane Appleseed"
-                />
-                <label 
-                    htmlFor="number"
-                    className="label"
-                >Card Number</label>
-                <input
-                    className="input"
-                    name="number"
-                    id="number"
-                    placeholder="e.g. 1234 5678 9123 0000"
-                />
-                <div className="form--expDate--cvc--div">
-                    <div className="form--expDate--label--div">
-                        <label
-                            className="label"
-                        >Exp. Date (MM/YY)</label>
-                        <div className="form--expDate--div">
-                            <input
-                                className="input"
-                                name="MM"
-                                id="MM"
-                                placeholder="MM"
-                            />
-                            <input
-                                className="input"
-                                name="YY"
-                                id="YY"
-                                placeholder="YY"
-                            />
-                        </div>
-                    </div>
-                    <div className="form--CVC--div">
-                        <label
-                            className="label" 
-                            htmlFor="e.g."
-                        >CVC</label>
-                        <input
-                            className="input"
-                            name="eg"
-                            id="eg"
-                            placeholder="e.g. 123"
-                        />
-                    </div>
-                </div>
-                <button
-                    className="btn"
-                    id="btn"
-                    name="btn"
-                >Confirm</button>
-            </form>
+            <Cards />
+            <Form />
         </div>
     )
 }
