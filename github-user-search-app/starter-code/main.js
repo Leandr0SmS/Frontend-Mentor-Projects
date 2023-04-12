3fde65cb3550318d08d86f970f84a854ee984500
Leandro_SmS <leandrosimoesms@gmail.com>
2023-04-12 10:06:58 -0300

    Set App and render

--- github-user-search-app/starter-code/main.js
+++ github-user-search-app/starter-code/main.js
@@ -14,3 +14,19 @@
   Following
 */
 
+function App() {
+    return (
+        <div className="app">
+            <header className="header">
+                <h1 className="title">devfinder</h1>
+                
+            </header>
+            <div className="search"></div>
+        </div>
+    )
+}
+
+//Render
+const app = document.getElementById('root');
+const root = ReactDOM.createRoot(app);
+root.render(<App />)
