30be4c0a6d122a59cc93df28c6a664c10899c74f
Leandro_SmS <leandrosimoesms@gmail.com>
2023-04-14 10:32:35 -0300

    main.js: Set color mode

--- github-user-search-app/starter-code/main.js
+++ github-user-search-app/starter-code/main.js
@@ -15,13 +15,48 @@
 */
 
 function App() {
+    const [colorMode, setColorMode] = React.useState(true);
+    function handleColorMode() {
+        setColorMode(p => !p)
+    }
+
+    console.log(colorMode)
     return (
-        <div className="app">
+        <div className={colorMode ? "app" : "app dark-mode"}>
             <header className="header">
                 <h1 className="title">devfinder</h1>
-                
+                {
+                    colorMode
+                    ? <div 
+                        className="color-mode-div"
+                        onClick={handleColorMode}
+                      >
+                        <p className="color-mode-text">Dark</p>
+                        <img
+                            className="moon-icon"
+                            src="./assets/icon-moon.svg"
+                            alt="moon icon to dark colors mode"
+                            aria-hidden="false"
+                            role="img"
+                        />
+                      </div>
+                    : <div 
+                        className="color-mode-div"
+                        onClick={handleColorMode}
+                      >
+                        <p className="color-mode-text">Light</p>
+                        <img
+                            className="sun-icon"
+                            src="./assets/icon-sun.svg"
+                            alt="sun icon to dark colors mode"
+                            aria-hidden="false"
+                            role="img"
+                        />
+                      </div>
+                }
             </header>
-            <div className="search"></div>
+            <div className="search">
+            </div>
         </div>
     )
 }
