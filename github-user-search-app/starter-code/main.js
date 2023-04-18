8678cf0d005972e12ed27d13d75048ced846379e
Leandro_SmS <leandrosimoesms@gmail.com>
2023-04-18 07:07:04 -0300

    main.js: Set error state

--- github-user-search-app/starter-code/main.js
+++ github-user-search-app/starter-code/main.js
@@ -5,15 +5,15 @@ function dateFormat(inputDate) {
     const month = monthNames[date.getUTCMonth()];
     const year = date.getUTCFullYear();
     return `${day} ${month} ${year}`;
-}
+};
 
 function App() {
 
     //State to dark mode
     const [colorMode, setColorMode] = React.useState(true);
     function handleColorMode() {
-        setColorMode(p => !p)
-    }
+        setColorMode(p => !p);
+    };
     // Looknig for color mode of the user
     React.useEffect(() => {
         const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
@@ -23,21 +23,25 @@ function App() {
     //Form State
     const [formData, setFormData] = React.useState("");
     function handleChange(e) {
-        setFormData(e.target.value)
-    }
+        setFormData(e.target.value);
+        setError(false);
+    };
     //Data State
     const [data, setData] = React.useState("");
+    //Error state
+    const [error, setError] = React.useState(false);
     // State to trigger fetch data from API
     const [get, setGet] = React.useState(0);
     function handleClick() {
-        setGet(get => get + 1)
+        setGet(get => get + 1);
+        data.message === "Not Found" && setError(true);
     };
     //Fetch data
     React.useEffect(() => {
         fetch(`https://api.github.com/users/${formData ? formData : "octocat"}`)
             .then(res => res.json())
             .then(data => setData(data))
-    }, [get])
+    }, [get]);
 
     return (
         <div className={colorMode ? "bg" : "bg-dark bg"}>
