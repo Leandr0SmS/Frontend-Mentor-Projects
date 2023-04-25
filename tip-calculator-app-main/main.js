c3855ed2a7a95d9571c9b6f4e85f8a134d836b84
Leandro_SmS <leandrosimoesms@gmail.com>
2023-04-25 17:31:09 -0300

    set Calculator and App Components

--- tip-calculator-app-main/main.js
+++ tip-calculator-app-main/main.js
@@ -14,3 +14,28 @@ Total
 / person
 Reset
 */
+
+const Calculator = () => {
+    return (
+        <div className="calculator"></div>
+    )
+}
+
+const App = () => {
+    return (
+        <div className="app">
+            <img
+                className="logo"
+                src="./images/logo.svg"
+                alt="logo of splitter"
+                aria-hidden="false"
+                role="img"
+            />
+            <Calculator/>
+        </div>
+    )
+}
+
+const app = document.getElementById('root');
+const root = ReactDOM.createRoot(app);
+root.render(<App/>)
