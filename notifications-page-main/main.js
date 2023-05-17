35cdef2204ebed36eb9aef49e68c950163522523
Leandro_SmS <leandrosimoesms@gmail.com>
2023-05-17 06:49:09 -0300

    set data to an object to genereate notifications

--- notifications-page-main/main.js
+++ notifications-page-main/main.js
@@ -1,35 +1,57 @@
-/*
-  Notifications 3
+const notificationData = [
+    {
+        id: 01,
+        user: "Mark Webber",
+        action: "reacted to your recent post",
+        element: "My first tournament today!",
+        time: "1m ago"
+    },
+    {
+        id: 02,
+        user: "Angela Gray",
+        action: "followed you",
+        element: "",
+        time: "5m ago"
+    },
+    {
+        id: 03,
+        user: "Jacob Thompson",
+        action: "has joined your group",
+        element: "Chess Club",
+        time: "1 day ago"
+    },
+    {
+        id: 04,
+        user: "Rizky Hasanuddin",
+        action: "sent you a private message",
+        element: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+        time: "5 days ago"
+    },
+    {
+        id: 05,
+        user: "Kimberly Smith",
+        action: "commented on your picture",
+        element: "",
+        time: "1 week ago"
+    },
+    {
+        id: 06,
+        user: "Nathan Peterson",
+        action: "reacted to your recent post",
+        element: "5 end-game strategies to increase your win rate",
+        time: "2 weeks ago"
+    },
+    {
+        id: 07,
+        user: "Anna Kim",
+        action: "left the group",
+        element: "Chess Club",
+        time: "2 weeks ago"
+    },
+]
 
-  Mark all as read
-
-  Mark Webber reacted to your recent post My first tournament today!
-  1m ago
-
-  Angela Gray followed you
-  5m ago
-
-  Jacob Thompson has joined your group Chess Club
-  1 day ago
-
-  Rizky Hasanuddin sent you a private message
-  5 days ago
-  Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
-  I'm already having lots of fun and improving my game.
-
-  Kimberly Smith commented on your picture
-  1 week ago
-
-  Nathan Peterson reacted to your recent post 5 end-game strategies to increase your win rate
-  2 weeks ago
-
-  Anna Kim left the group Chess Club
-  2 weeks ago
-*/
-
-const App = () => {
+const Header = () => {
     return (
-        <React.Fragment>
         <div className="header">
             <div className="notifications--count">
                 <h1>Notifications</h1>
@@ -37,6 +59,21 @@ const App = () => {
             </div>
             <div className="all-read--div">Mark all as read</div>
         </div>
+    )
+}
+
+const Notification = () => {
+    return (
+        <div className="notification">
+
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <Header/>
         </React.Fragment>
     )
 };
