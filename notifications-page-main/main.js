7b749c1e164fc5a5b8ce5bfa423396d07f88b528
Leandro_SmS <leandrosimoesms@gmail.com>
2023-05-17 06:55:10 -0300

    Set images src to notifications data

--- notifications-page-main/main.js
+++ notifications-page-main/main.js
@@ -2,6 +2,7 @@ const notificationData = [
     {
         id: 01,
         user: "Mark Webber",
+        img: "./assets/images/avatar-mark-webber.webp",
         action: "reacted to your recent post",
         element: "My first tournament today!",
         time: "1m ago"
@@ -9,6 +10,7 @@ const notificationData = [
     {
         id: 02,
         user: "Angela Gray",
+        img: "./assets/images/avatar-angela-gray.webp",
         action: "followed you",
         element: "",
         time: "5m ago"
@@ -16,6 +18,7 @@ const notificationData = [
     {
         id: 03,
         user: "Jacob Thompson",
+        img: "./assets/images/avatar-jacob-thompson.webp",
         action: "has joined your group",
         element: "Chess Club",
         time: "1 day ago"
@@ -23,6 +26,7 @@ const notificationData = [
     {
         id: 04,
         user: "Rizky Hasanuddin",
+        img: "./assets/images/avatar-rizky-hasanuddin.webp",
         action: "sent you a private message",
         element: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
         time: "5 days ago"
@@ -30,6 +34,7 @@ const notificationData = [
     {
         id: 05,
         user: "Kimberly Smith",
+        img: "./assets/images/avatar-kimberly-smith.webp",
         action: "commented on your picture",
         element: "",
         time: "1 week ago"
@@ -37,6 +42,7 @@ const notificationData = [
     {
         id: 06,
         user: "Nathan Peterson",
+        img: "./assets/images/avatar-nathan-peterson.webp",
         action: "reacted to your recent post",
         element: "5 end-game strategies to increase your win rate",
         time: "2 weeks ago"
@@ -44,6 +50,7 @@ const notificationData = [
     {
         id: 07,
         user: "Anna Kim",
+        img: "./assets/images/avatar-anna-kim.webp",
         action: "left the group",
         element: "Chess Club",
         time: "2 weeks ago"
@@ -65,7 +72,6 @@ const Header = () => {
 const Notification = () => {
     return (
         <div className="notification">
-
         </div>
     )
 }
