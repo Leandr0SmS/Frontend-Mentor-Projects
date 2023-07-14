4034a038c55525a9a31c84d68d4693c58654cf67
Leandro_SmS <leandrosimoesms@gmail.com>
2023-07-14 06:38:53 -0300

    Set buttons

--- coding-bootcamp-testimonials-slider-master/data/data.js
+++ coding-bootcamp-testimonials-slider-master/data/data.js
@@ -2,11 +2,13 @@ export const users_data = [
     {
         user_name: 'Tanya Sinclair',
         role: 'UX Engineer',
-        text: "I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future."
+        text: "I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future.",
+        src:'image-tanya.jpg'
     },
     {
         user_name: 'John Tarkpor',
         role: 'Junior Front-end Developer',
-        text: "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."
+        text: "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+        src:'image-john.jpg'
     }
 ]
--- coding-bootcamp-testimonials-slider-master/main.js
+++ coding-bootcamp-testimonials-slider-master/main.js
@@ -2,19 +2,55 @@ import { users_data } from "./data/data.js";
 const { useState } = React;
 const { createRoot } = ReactDOM;
 
-const Slide = ({ text, image, user_name, role }) => {
+const Slide = ({ text, image_src, user_name, role }) => {
     <React.Fragment>
-            <div id="text"></div>
-            <div id="hero-img"></div>
+            <div className="text">
+                <p className="testemonial">{text}</p>
+            </div>
+            <div className="hero-img-div">
+                <img
+                    className="hero-img"
+                    src={`./images/${image_src}`}
+                    alt={`image of ${user_name}`}
+                    aria-hidden="false"
+                    role="img"
+                />
+            </div>
     </React.Fragment>
 }
 
 const App = () => {
 
+    const [slide, setSlide] = useState(0);
+
     return (
           <div id="app">
-            <div id="text"></div>
-            <div id="hero-img"></div>
+            <div id="btns">
+                <button
+                    id="prev-btn"
+                    type="button"
+                >
+                    <img
+                        id="prev-icon"
+                        src="./images/icon-prev.svg"
+                        alt="preview icon"
+                        aria-hidden="false"
+                        role="img"
+                    />
+                </button>
+                <button
+                    id="next-btn"
+                    type="button"
+                >
+                    <img
+                        id="next-icon"
+                        src="./images/icon-next.svg"
+                        alt="next icon"
+                        aria-hidden="false"
+                        role="img"
+                    />
+                </button>
+            </div>
           </div>
     )
 };
