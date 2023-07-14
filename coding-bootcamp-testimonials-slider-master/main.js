aef33b850c328810fd3cfe357f34dad77fb68b5c
Leandro_SmS <leandrosimoesms@gmail.com>
2023-07-14 06:21:40 -0300

    Set slide component

--- coding-bootcamp-testimonials-slider-master/main.js
+++ coding-bootcamp-testimonials-slider-master/main.js
@@ -1,12 +1,21 @@
+import { users_data } from "./data/data.js";
 const { useState } = React;
 const { createRoot } = ReactDOM;
 
-const App = () => {
-    return (
-          <React.Fragment>
+const Slide = ({ text, image, user_name, role }) => {
+    <React.Fragment>
             <div id="text"></div>
             <div id="hero-img"></div>
-          </React.Fragment>
+    </React.Fragment>
+}
+
+const App = () => {
+
+    return (
+          <div id="app">
+            <div id="text"></div>
+            <div id="hero-img"></div>
+          </div>
     )
 };
 
